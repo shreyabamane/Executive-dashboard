4b041d6d08af2a564c8bf1a69065d3adc625b5d3
Shreya Prakash Bamane <shreyabamane123@gmail.com>
2024-08-27 17:22:02 +0530

    modify

--- src/components/MyChart/MyChart.jsx
+++ src/components/MyChart/MyChart.jsx
@@ -13,9 +13,9 @@ const MyChart = () => {
       canvas.chartInstance.destroy();
     }
 
-    const xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
-    const yValues = [55, 49, 44, 24, 15];
-    const barColors = ["#b91d47", "#00aba9", "#2b5797", "#e8c3b9", "#1e7145"];
+    const xValues = ["Failed", "Warning", "Not available", "Pass"];
+    const yValues = [1689, 681, 36, 7253];
+    const barColors = ["#8B0000", "#FF8C00", "#A9A9A9", "#228B22"];
 
     const newChart = new Chart(ctx, {
       type: "doughnut",
